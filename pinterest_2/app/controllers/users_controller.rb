e517fc0751e922f439ebe501b2a748bc6935d8cd
Justin <justinhwu95@gmail.com>
2019-05-02 10:12:05 -0400

    Added migrations and MVC

---
+++ pinterest_2/app/controllers/users_controller.rb
@@ -0,0 +1,52 @@
+class UsersController < ApplicationController
+  before_action :find_user, only: [:show, :update, :edit]
+
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.invalid?
+      render 'new'
+    else
+      @user.save
+      redirect_to user_path(@user)
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @user.assign_attributes(user_params)
+    if @user.invalid?
+      render 'edit'
+    else
+      @user.save
+      redirect_to user_path(@user)
+    end
+  end
+
+  def destroy
+    User.destroy(params[:id])
+    redirect_to users_path
+  end
+
+  private
+  def user_params
+    params.require(:user).permit!
+  end
+
+  def find_user
+    @user = User.find(params[:id])
+  end
+
+end
---
+++ pinterest_2/app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  has_many :recipes
+  has_many :allergens
+  has_many :ingredients, through: :allergens
+  validates :name, presence: true
+
+end
